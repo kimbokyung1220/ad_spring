a5b5c5e8dce7ca21f40166fc195228f9caf53788
bo kyung <bkyungkeem@daum.net>
2023-03-09 17:56:13 +0900

    feat) api 변경

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ItemController {
     private final ItemService itemService;
     @PostMapping("/item")
